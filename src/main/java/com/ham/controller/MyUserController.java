0152eeb1cee1d0b4a0fedb36f53a4a329886a75a
sorrel012 <wain1719@gmail.com>
2023-07-09 21:38:16 +0900

    import 수정

--- src/main/java/com/ham/controller/MyUserController.java
+++ src/main/java/com/ham/controller/MyUserController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
